21430ecbbf58c2b5bac423844e0fa3fe5e0f4ca8
marovichn <nikolamarovic.11@gmail.com>
2023-04-24 23:06:19 +0200

    clear-code-types-constructors

--- 2-ts-prject/src/App.tsx
+++ 2-ts-prject/src/App.tsx
@@ -1,9 +1,14 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("Learn"), new Todo("TypeScript")];
+
   return (
     <div className="App">
-      <Todos></Todos>
+      <Todos items={todos}>
+        <p>Hi</p>
+      </Todos>
     </div>
   );
 }
--- 2-ts-prject/src/components/Todos.tsx
+++ 2-ts-prject/src/components/Todos.tsx
@@ -1,10 +1,17 @@
-function Todos() {
+import React from "react";
+import Todo from "../models/todo";
+
+const Todos: React.FC<{ children: React.ReactNode; items: Todo[] }> = (
+  props
+) => {
   return (
     <ul>
-      <li>Learn React</li>
+      {props.items.map((i) => (
+        <li key={i.id}>{i.text}</li>
+      ))}
       <li>Learn TypeScript</li>
     </ul>
   );
-}
+};
 
 export default Todos;
